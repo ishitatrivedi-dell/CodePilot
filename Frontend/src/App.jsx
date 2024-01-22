2d096fa291aa2cf3aa28ddc2a3c3cc37a8c6a709
Hritik262 <hritikraj121@gmail.com>
2024-01-22 14:44:05 +0530

    Add features component of the CodePilot

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import HeroSection from './components/HeroSection.jsx'
+import Features from './components/Features.jsx'
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,6 +19,7 @@ function App() {
       <Router>
         <Navbar/>
         <HeroSection/>
+        <Features/>
         <Footer/>
       </Router>
     </>
