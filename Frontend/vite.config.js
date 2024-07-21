38da009b05ac5d67e333bc1be3742d9b04b82652
Hritik262 <hritikraj121@gmail.com>
2024-07-21 14:57:11 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://code-pilot-backend.vercel.app',
+      '/api':'https://codepilot-9q4e.onrender.com',
     },
   },
   plugins: [react()],
